6eb24222fc75b9884345aad63edfc92feab311c3
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2024-01-17 14:45:18 +0100

    update files E7
    update files E7
    - se realiza de otra forma

---
+++ UT4/src/main/java/UT4/Ejercicios_Arrays/Ejercicio7b.java
@@ -0,0 +1,12 @@
+package UT4.Ejercicios_Arrays;
+
+public class Ejercicio7b {
+
+	public static void main(String[] args) {
+		
+		int [][ ]mat = {{1,5,3},{2,5,8},{5,6,1}};
+		Ejercicio7.mostrarMatriz(mat, "array original");
+		
+	}
+
+}
